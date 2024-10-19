c84c262432b7e33c1eacce17d71b3080a6c406ce
HailieTapia <136946909+HailieTapia@users.noreply.github.com>
2024-10-19 13:47:26 -0600

    new file:   src/zxcvbn.d.ts 	modified:   tsconfig.app.json 	deleted:    tsconfig.spec.json

---
+++ src/zxcvbn.d.ts
@@ -0,0 +1,24 @@
+declare module 'zxcvbn' {
+    export default function zxcvbn(password: string): {
+      score: number;
+      feedback: {
+        suggestions: string[];
+        warning: string;
+      };
+      guesses: number;
+      guesses_log10: number;
+      crack_times_seconds: {
+        online_throttling_100_per_hour: number;
+        online_no_throttling_10_per_second: number;
+        offline_slow_hashing_1e4_per_second: number;
+        offline_fast_hashing_1e10_per_second: number;
+      };
+      crack_times_display: {
+        online_throttling_100_per_hour: string;
+        online_no_throttling_10_per_second: string;
+        offline_slow_hashing_1e4_per_second: string;
+        offline_fast_hashing_1e10_per_second: string;
+      };
+    };
+  }
+  
